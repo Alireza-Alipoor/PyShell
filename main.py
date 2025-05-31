5feb0b79631acebfa0010ea168f7bb4da519333f
Alireza-Alipoor <thealipoor@gmail.com>
2025-05-31 17:20:55 +0330

    feat: implement handle_input

--- main.py
+++ main.py
@@ -1,12 +1,58 @@
 import sys
+import shutil
+import shlex
+import subprocess
+
+COMMANDS = {
+    "exit": lambda args: handle_exit(args),
+    "echo": lambda args: handle_echo(args),
+    "type": lambda args: handle_type(args),
+    "pwd": lambda args: handle_pwd(args),
+    "cd": lambda args: handle_cd(args)}
+
+
+def handle_input(input: str) -> None:
+    command, *args = shlex.split(input)
+    if command in COMMANDS:
+        COMMANDS[command](args)
+    elif shutil.which(command):
+        subprocess.run([command, *args], capture_output=True,
+                       text=True,
+                       stderr=sys.stderr,
+                       stdout=sys.stdout)
+    else:
+        sys.stdout.write(f'{command}: command not found\n')
+
+
+def handle_exit():
+    ...
+
+
+def handle_echo():
+    ...
+
+
+def handle_type():
+    ...
+
+
+def handle_pwd():
+    ...
+
+
+def handle_cd():
+    ...
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = sys.stdin.readline().rstrip('\n')
-        sys.stdout.write(f'{command}: command not found')
+        try:
+            input = sys.stdin.readline()
+            handle_input(input)
+        except ValueError:
+            continue
 
 
 if __name__ == "__main__":
